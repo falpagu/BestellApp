c4eeb09ae1ae7b843f0282fb1bd09e170bb21087
Fesih Alpagu <alpagu.fesih@gmx.at>
2026-03-19 11:05:19 +0100

    feat: add js and css

--- main.js
+++ main.js
@@ -1,49 +1,210 @@
-function init() {
-    renderMenu();
-    renderBasket();
-
-}
-
+let basketOpened = false;
 let basket = [];
 
-function renderMenu() {
-let allMenuRef = document.getElementById('menu');
-let html = "";
+let orderType = "delivery";
+let DeliveryFee = 3.99;
 
-for (let i = 0; i < allMenu.length; i++) {
+function init() {
+  renderMenu();
+  renderBasket();
+  saveToLocalStorage();
+}
+
+function renderMenu() {
+  let allMenuRef = document.getElementById("menu");
+  let html = "";
+
+  for (let i = 0; i < allMenu.length; i++) {
     html += getCategoryTemplate(i);
 
     for (let j = 0; j < allMenu[i].dishes.length; j++) {
-
-        html += getDishTemplate(i, j);
-        }
+      html += getDishTemplate(i, j);
     }
-
-    allMenuRef.innerHTML = html;
+  }
+  allMenuRef.innerHTML = html;
 }
 
+function openBasket() {
+  basketOpened = true;
 
-function addToBasket(id) {
-     for (let i = 0; i < allMenu.length; i++) {
-    let dish = allMenu[i].dishes.find(d=> d.id === id); 
+  renderBasket();
+  switchBtn("delivery");
+}
+
+function closeBasketBtn() {
+  document.getElementById("basket").style.display = "none";
+}
+
+function addToBasket(event, category, id) {
+  openBasket();
+
+  let dish = allMenu[category].dishes.find((d) => d.id === id);
+
+  if (dish) {
+    dish.amount++;
     if (!basket.includes(dish)) {
-        basket.push(dish);
+      basket.push(dish);
     }
- console.log(basket);
+    let button = event.target;
+    button.innerHTML = `Added ${dish.amount}`;
+    button.classList.add("addedItem");
+  }
 
-    }
-renderBasket();
+  renderBasket();
+  switchBtn("delivery");
 }
 
-   
-function renderBasket() {
- let basketRef = document.getElementById('basket');
-    let html = "";
+function updateDishAmount() {
+  let numberOfDishes = basket.reduce((sum, dish) => sum + dish.amount, 0);
+  document.getElementById("dishAmount").innerHTML = numberOfDishes;
+}
 
-    for (let i = 0; i < basket.length; i++) {
-        let dish = basket[i];
-     html += getBasketTemplate(dish);
-        
-    }
-    basketRef.innerHTML = html;
+function renderBasket() {
+  let basketRef = document.getElementById("basket");
+
+  if (!basketOpened) {
+    basketRef.innerHTML = "";
+    // don't do anything when basket is close
+    return;
+  }
+  let html = `<div class="basket">
+     
+  <div class="basketBtnContainer">
+    <img onclick="closeBasketBtn()"class="closeBasketBtn" src="./assets/icons-logos/shopping-cart.svg">
+    <span id="dishAmount" class="dishAmount">0</span>
+  </div>
+      
+  <h2>Your Basket</h2>
+  
+      <div class="deliveryCollection">
+            <button id="delivery" onclick="switchBtn('delivery')"><img src="./assets/icons-logos/delivery-on-the-way.svg">Delivery</button>
+            <button id="collection" onclick="switchBtn('collection')">🛍️Collection</button>
+      </div>
+  `;
+
+  if (basket.length === 0) {
+    html += `
+    <div class="basketEmty">
+          <h4>Fill your basket</h4>
+          <span>Your basket is empty</span>
+          <img src="./assets/icons-logos/shopping-cart.png">
+        </div>`;
+  } else {
+    html += `<div class="basketDishes">`;
+    html += renderBasketItems();
+    html += `</div>`;
+    html += renderBasketSum();
+  }
+  html += `</div>`;
+
+  basketRef.innerHTML = html;
+  updateDishAmount();
+}
+
+function renderBasketItems() {
+  let html = "";
+
+  for (let i = 0; i < basket.length; i++) {
+    let dish = basket[i];
+    html += getBasketItemsTemplate(dish);
+  }
+
+  return html;
+}
+
+function renderBasketSum() {
+  let html = "";
+
+  let subtotal = 0;
+
+  for (let i = 0; i < basket.length; i++) {
+    subtotal += basket[i].price * basket[i].amount;
+  }
+
+  let delivery = DeliveryFee;
+  let total = subtotal + delivery;
+
+  html = getSumTemplate(subtotal, delivery, total);
+  return html;
+}
+
+function switchBtn(type) {
+  orderType = type;
+
+  if (type === "delivery") {
+    DeliveryFee = 3.99;
+  } else {
+    DeliveryFee = 0;
+  }
+
+  renderBasket();
+
+  let delivery = document.getElementById("delivery");
+  let collection = document.getElementById("collection");
+  let selected = document.getElementById(type);
+
+  if (delivery && collection && selected) {
+    delivery.classList.remove("active");
+    collection.classList.remove("active");
+    selected.classList.add("active");
+  }
+}
+
+function decrement(id) {
+  let dish = basket.find((d) => d.id === id);
+  if (!dish) return;
+  dish.amount--;
+  if (dish.amount <= 0) {
+    basket = basket.filter((d) => d.id !== id);
+  }
+  saveToLocalStorage();
+  renderBasket();
+}
+
+function increment(id) {
+  let dish = basket.find((d) => d.id === id);
+  if (!dish) return;
+
+  dish.amount++;
+  saveToLocalStorage();
+  renderBasket();
+}
+
+function deleteDish(id) {
+  basket = basket.filter((d) => d.id !== id);
+  renderBasket();
+}
+
+function buyNow() {
+  basketOpened = false; //Basket get closes
+  renderBasket();
+
+  let main = document.querySelector("main");
+  main.innerHTML += getOrderConfirmedTemplate();
+
+  let dialog = document.getElementById("orderConfirmContainer");
+  dialog.showModal();
+
+  setTimeout(() => {
+    dialog.close();
+    dialog.remove();
+  }, 5000);
+}
+
+function orderConfirmCloseBtn() {
+  let dialog = document.getElementById("orderConfirmContainer");
+  dialog.close();
+  dialog.remove();
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("basket", JSON.stringify(basket));
+}
+function getFromLocalStorage() {
+  let storedBasket = JSON.parse(localStorage.getItem("basket"));
+
+  if (storedBasket !== null) {
+    basket = storedBasket;
+  }
+  renderBasket();
 }
--- template.js
+++ template.js
@@ -1,31 +1,42 @@
+
 function getCategoryTemplate(i) {
 let category = allMenu[i];
 
+  let text = category.category.replace(
+    /\(.*?\)/,
+    '<span class="size">$&</span>'
+  );
+
 return `
-    <div class="category">    
-       <img src="${category.icon}">
-       <h2>${category.category}</h2>
-    </div>      
+      <div class="categoryContainer">
+        <div class="category">   
+          <img src="${category.icon}">
+          <h2>${text}</h2>
+        </div> 
+      </div>   
 `;   
 
 }
 
-
 function getDishTemplate(i, j) {
 let dish = allMenu[i].dishes[j]
 
-return `
-    <div class="DishCard">
-        <div><img src="${dish.img}"></div>
-            <div class="dishName">
-                <span>${dish.name}</span>
-                <p>${dish.ingredients}</p> 
-        </div>
 
-        <div class="priceAdd" >
+
+return `
+    <div class="dishCard">
+        <div onclick="openBasket()"><img src="${dish.img}"></div>
+      <div class="dishCardContent">
+            <div class="dishName">
+                <span onclick="openBasket()">${dish.name}</span>
+                <p>${dish.ingredients}</p> 
+            </div>
+
+            <div class="priceAdd" >
                 <span>${dish.price}€</span>
-                <button onclick="addToBasket(${dish.id})">Add to basket</button> 
-        </div>        
+                <button id="addButton" onclick="addToBasket(event, ${i}, ${dish.id})">Add to basket</button> 
+            </div>   
+      </div>       
         
     </div>
        
@@ -33,45 +44,76 @@ return `
 
 }
 
+function getBasketItemsTemplate(dish) {
 
-function getBasketTemplate(dish) {
+  if (!dish) return ''
 
-    return `
-             <div class="basket">
-            <h2>Your Basket</h2>
-            <div >
-              <div class="basketCard">
-                <div>
-                  <span>${dish.amount} x</span>
-                  <span>${dish.name}</span>
-                </div>
+  let minusIcon = dish.amount === 1
 
-                <div class="basketContent">
+  ? `<img onclick="decrement(${dish.id})" src="./assets/icons-logos/delete-icon.svg">`
+  : `<i onclick="decrement(${dish.id})" class="bi bi-dash-lg"></i> 
+  `;
+  
+  let deleteIcon = dish.amount > 1
+  ? `<button onclick="deleteDish(${dish.id})">
+  <img src="./assets/icons-logos/delete-icon.svg"></button>`
+  : '';
+
+  return `
+    <div>
+              <div class="basketBox">
+              <div class="nameDeleteBtn">
+                <div>${dish.name}</div>
+                ${deleteIcon}
+              </div>   
+
+                <div class="basketAmountPreis">
+
                   <div class="deleteAdd">
-                    <span>
-                      <img class="decrement"
-                        src="./assets/icons-logos/delete-icon.svg"
-                        alt="delete button"
-                    /></span>
-                    <span class="decrement">${dish.amount}</span>
-                    <span class="increment">➕</span>
+                  ${minusIcon}
+                   <span class="amount">${dish.amount}</span>
+                   <i onclick="increment(${dish.id})" class="bi bi-plus-lg"></i>
                   </div>
 
-                  <div>${dish.price * dish.amount}€</div>
+                  <div>${(dish.price * dish.amount).toFixed(2)}€</div>
                 </div>
-              </div>
 
-              <div class="priceTotal">
-                <p class="priceLine"><span>Subtotal</span> <span>444</span></p>
-                <p class="priceLine">
-                  <span>Delivey fee</span> <span>444</span>
-                </p>
-                <div class="separator"></div>
-                <p class="priceLine"><span>Total</span><span>444</span></p>
               </div>
+    </div>
 
-              <button class="buyNow">Buy now (444€)</button>
-            </div>
-        </div>
     `;
 }
+
+function getSumTemplate(subtotal, delivery, total){
+
+  return `
+             <div class="sumContainer">
+              <div class="sum">
+                <p class="priceLine"><span>Subtotal:</span><span>${subtotal.toFixed(2)}€</span></p>
+                <p class="priceLine">
+                  <span>Delivery fee:</span><span>${delivery}€</span>
+                </p>
+                <div class="separator"></div>
+                <p class="priceLine"><span>Total:</span><span>${total.toFixed(2)}€</span></p>
+              </div>
+              <button class="buyNow"  onclick="buyNow()">Buy now (${total.toFixed(2)}€)</button>
+            </div>
+  `;
+}
+
+function getOrderConfirmedTemplate(){
+
+  return `
+          <dialog id="orderConfirmContainer" class="orderConfirmContainer">
+            <button id="orderConfirmCloseBtn" class="orderConfirmCloseBtn" onclick="orderConfirmCloseBtn()">&times;</button> 
+              <div class="orderConfirm">
+                <img src="./assets/icons-logos/delivery-on-the-way.svg">
+                <p>Order confirmed!</p>
+                <span>Your food is on the way!</span>
+            </div>
+          </dialog>
+
+  `;
+}
+
+    
